34504d94963de684688ad7194f3567c956432714
Abhishek Singh <abk90133@gmail.com>
2022-08-17 14:46:44 +0530

    Added Passport, sessions, authenticate the user and valid the userlogin

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//jshint esversion:6
+  //jshint esversion:6
 require('dotenv').config();
 const express = require("express");
 
@@ -7,23 +7,43 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption");
 // const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+//PASSPORT NOT ONLY ADD COOKIES BUT ALSO REMEMBERS WHAT THE USER IS UPTO IN THE WEB PAGE, which is known as the "SESSION"
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 
 const app = express();
+
+//this has to be mentioned because of the express not allow any other CSS, IMages or any other folder to run, so we have to mention each and every file on the Public folder explicitally
+app.use(express.static("public"));
+//this has to be mentioned because of the express not allow any other CSS, IMages or any other folder to run, so we have to mention each and every file on the Public folder explicitally
+
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static("public"));
-//this has to be mentioned because of the express not allow any other CSS, IMages or any other folder to run, so we have to mention each and every file on the Public folder explicitally
 
-//connecting to the MongoDB server, this initially requires the 3 steps:
+//this is used to save the session, As stated here it is using the session package i.e const session = require("express-session");
+app.use(session({
+  secret: "We have to remember this for future reference",
+  resave: false,
+  saveUninitialized: false
+}));
 
+//MOST IMPORTANT IS THAT HERE THE PLACEMENT OF THE CODE BELOW THE LINES IS VERY IMPORTANT BECAUSE JS EXECUTE THE CODE LINE BY LINES
+//SO INITILAIZING THE PASSPORT AND THE COOKIES IS VERY MUCH STATED HERE
+
+//can be seen at Passport.org/documentation/session
+app.use(passport.initialize());
+app.use(passport.session());
+
+//connecting to the MongoDB server
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
+
+//initializing a schema to our Mongodb so that we can declare a schema and use it on the server
 const userSchema = new mongoose.Schema ({ //this type of mongoose encryption is used when we want to encrypt a data
   email: String,
   password: String
@@ -33,7 +53,22 @@ const userSchema = new mongoose.Schema ({ //this type of mongoose encryption is 
 //we have removed the above line of code because of the MD5 hash function which we have used which will help us to Hash a function instread of
 // encrypting it
 
+//this plugin is used to Encrypt the Mongoose, I.ee Hashing and Salting of the data we will be putting into the mongoose
+userSchema.plugin(passportLocalMongoose);
+
+
 const User = new mongoose.model("User", userSchema);
+
+
+//This is used for the Locall configuration of the passport
+passport.use(User.createStrategy());
+
+//What this searialize and deserealise is doing is it is adding the information of the user to the cookies so that it can Authenticate the user information when they are present and when the
+//info is deleted
+//they are also important for the security purpose also because they will be terminating you when you have exited the code.
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/", function(req, res) {
   res.render("home");
@@ -49,39 +84,66 @@ app.get("/register", function(req, res) {
 
 //Used for registering the user to the User Database of MongoDB
 
-app.post("/register", function(req, res){
-  //added the bcrypt that will increase the number of rounds of hashes which added the security to our "PASSWORD" only.
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
+//This is our most Confidential page that will only allow us get in only when the ser is asking to get inside it is "AUTHENTICATED".
+app.get("/secrets", function(req, res){
+  if(req.isAuthenticated()){
+    //IF WE ARE ON THE SAME PAGE THEN WE USE THE RES.RENDER ELSE WE USE THE RES.REDIRECT
+    //AS WE CAN SEE HERE WE ARE AGAIN REDIRECTING THEM TO THE SAME PAGE
+    res.render("secrets");
+  }else {
+    res.redirect("/login");
+  }
+});
+
+// app.get("/logout", function(req, res){
+//   req.logout();
+//   res.redirect("/");
+// });
+
+app.get("/logout", (req, res) => {
+  req.logout(req.user, err => {
+    if(err) return next(err);
+    res.redirect("/");
   });
-  newUser.save(function(err){
+});
+
+app.post("/register", function(req, res){
+  User.register({username: req.body.username}, req.body.password, function(err, user){
     if(err){
       console.log(err);
-    } else {
-      res.render("secrets");
+      res.redirect("/register");
+    } else{
+      //here we are Authenticating the user with the information he have added to this, and saving the cookies to the Local browser history
+      //It is not only saving the Browser history but also the cookies
+      passport.authenticate("local")(req, res, function(){
+        //IF EVERYTHING WENTS FINE THEN WE GO TO secrets
+        res.redirect("/secrets");
+      });
     }
   });
 });
 
-
-
 app.post("/login", function(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({email: username}, function(err, foundOne){
-    if(err){
-      console.log(err);
-    } else {
-      if(foundOne){
-        if(foundOne.password  === password){
-          res.render("secrets");
-        }
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+    //below is the passport package i.e req.login
+    req.login(user, function() {
+      if(err) {
+        console.log(err);
+      }else {
+        passport.authenticate("local")(req, res, function(){
+        res.redirect("secrets");
+      });
       }
-    }
+    });
+});
+
+app.post('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
   });
 });
 
@@ -93,3 +155,6 @@ app.listen(3000, function(){
 //to hyper terminal
 
 //some testing is done
+
+//MOST IMPORTANT WHENEVER THE SERVER IS STARTED AGAIN SO ALL THE COOKIES WHICH ARE SAVED ON THE SERVER "NOT ON THE BROWSER" ARE DELETED AND WE will
+//FORCED TO LOGIN BACK
